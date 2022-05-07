3c518de65715a6044e73e827f0db73b286e5e21b
mertcankiyak <70319395+mertcankiyak@users.noreply.github.com>
2022-05-07 23:56:53 +0300

    update login_view theme

--- lib/features/authentication/view/login_view.dart
+++ lib/features/authentication/view/login_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:strapi_provider/features/authentication/model/request_user_model.dart';
@@ -10,70 +12,112 @@ class LoginView extends StatelessWidget with FormValidate {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     final _loginViewModel = Provider.of<LoginViewModel>(context);
     return Scaffold(
+      backgroundColor: Color(0xFFEBE9F3),
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        backgroundColor: Colors.indigo,
+        backgroundColor: Color(0xFFEBE9F3),
         elevation: 0,
-        title: Text("Login"),
       ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: ListView(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  validator: emailValidation,
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: emailText,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  validator: passwordControl,
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      hintText: passwordText),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ElevatedButton(
-                    style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(
-                          Size(MediaQuery.of(context).size.width,
-                              MediaQuery.of(context).size.height * 0.07),
-                        ),
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.black45)),
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState!.save();
-                       await _loginViewModel.login(
-                          requestUserModel: RequestUserModel(
-                            email: _emailController.text.trim(),
-                            password: _passwordController.text.trim(),
-                          ),
-                        );
-                      }
-                    },
-                    child: Text(logIn)),
-              ),
+              Text("Hello Again", style: Theme.of(context).textTheme.headline3, textAlign: TextAlign.center,),
+              Text("Wellcome back you've been missed", style: Theme.of(context).textTheme.headline5, textAlign: TextAlign.center,),
+              
             ],
           ),
-        ),
+        ],
       ),
     );
   }
 }
+/* Center(
+            child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Container(
+                  width: 200.0,
+                  height: 200.0,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade200.withOpacity(0.5)),
+                  child: const Center(
+                    child: Text(
+                      'Frosted',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),*/
+/*
+* SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                      child: Container(
+                        width: 200,
+                        height: MediaQuery.of(context).size.height,
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200.withOpacity(0.5)
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        validator: emailValidation,
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: emailText,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        validator: passwordControl,
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            hintText: passwordText),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              minimumSize: MaterialStateProperty.all(
+                                Size(MediaQuery.of(context).size.width,
+                                    MediaQuery.of(context).size.height * 0.07),
+                              ),
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.black45)),
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              _formKey.currentState!.save();
+                             await _loginViewModel.login(
+                                requestUserModel: RequestUserModel(
+                                  email: _emailController.text.trim(),
+                                  password: _passwordController.text.trim(),
+                                ),
+                              );
+                            }
+                          },
+                          child: Text(logIn)),
+                    ),
+                  ],
+                ),
+              ),
+            ),*/
